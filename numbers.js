dc846b0dd34afc7c5ec437aa3430cd14cc545b44
Vineela Gude <vineela.gude@netenrich.com>
2020-05-11 20:36:25 +0530

    added numbers and operations on numbers

---
+++ numbers.js
@@ -0,0 +1,29 @@
+let radius = 10;
+
+const pi = 3.14;
+
+console.log(radius, pi);
+
+//Math operations like +, - , *, **, % (BIDMAS)
+
+let area = pi * radius**2;
+console.log(area);
+
+radius += 5; //radius = radius + 5
+console.log(radius);
+
+radius++ //radius = radous + 1
+console.log(radius);
+
+radius--;
+radius *= 2;
+ 
+console.log(radius);
+
+//NaN Not a Number
+console.log(2 / 'Hello');
+
+//number concatination
+let currentRadius = 'after many changes ' + radius + ' is the radius';
+console.log(currentRadius); 
+
